c8545bceebf8d6a53d8468be7fb679a366505621
Supasate Choochaisri <supasate.c@gmail.com>
2016-07-29 09:17:10 +0700

    Change default port to 5100 to be consistent with development ecosystem file

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && API_URI && API_USER
 // Setup app
 const app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5100));
 // Must be called before bodyParser
 app.use(xhub({ algorithm: 'sha1', secret: APP_SECRET }));
 app.use(bodyParser.urlencoded({ extended: false }));

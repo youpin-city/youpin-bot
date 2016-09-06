621c825a7d902019a83a903fcf5438acb46cf32b
heytitle <pat.chormai@gmail.com>
2016-09-06 14:29:13 +0700

    Fix jslint

--- scripts/i18n-generator.js
+++ scripts/i18n-generator.js
@@ -1,51 +1,54 @@
-var i18nConfig = require("config").get("i18n")
-var _ = require('lodash');
-var Promise =require('bluebird');
-var fs = Promise.promisifyAll(require("fs"));
-var saw = require('string-saw')
+/* eslint no-param-reassign: ["warn"] */
+const i18nConfig = require('config').get('i18n');
+const _ = require('lodash');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+const saw = require('string-saw');
 
-var files = ['youpin.js'];
+const files = ['youpin.js'];
 
-function extractKeysFromFile(file){
-  return fs.readFileAsync( file, "utf8")
-  .then( data => {
-    var keys = saw(data)
-    .split("\n")
-    .map( line => {
-      return saw(line).match(/(?:__n?\(['"])(.+?)(?:['"])\)/).toArray();
-    }).toArray();
+function extractKeysFromFile(file) {
+  return fs.readFileAsync(file, 'utf8')
+  .then(data => {
+    const keys = saw(data)
+      .split('\n')
+      .map(line => saw(line)
+        .match(/(?:__n?\(['"])(.+?)(?:['"])\)/)
+        .toArray()
+      )
+      .toArray();
     return _.flatten(keys);
   });
 }
 
-function updateKeysToFile( keys, lang ){
-    var file = i18nConfig.directory+"/"+lang+".json";
-    fs.readFileAsync( file, "utf8" )
-      .then( data => {
-        console.log("Updating " + file );
-        var oldKeys = JSON.parse(data);
-        var mergedKeys = _.merge( keys, oldKeys )
+function updateKeysToFile(keys, lang) {
+  const file = `i18nConfig.directory/${lang}.json`;
+  fs.readFileAsync(file, 'utf8')
+      .then(data => {
+        console.log(`Updating ${file}`);
+        const oldKeys = JSON.parse(data);
+        const mergedKeys = _.merge(keys, oldKeys);
         fs.writeFileAsync(file, JSON.stringify(mergedKeys, null, 4))
-          .then( (err) => {
-              if(err) console.log(err)
-              console.log("Done : " + file )
-          })
+          .then((err) => {
+            if (err) console.log(err);
+            console.log(`Done : ${file}`);
+          });
       });
 }
 
-Promise.map( files, extractKeysFromFile ).then( keys => {
-    keys = _.chain(keys)
+Promise.map(files, extractKeysFromFile).then(items => {
+  const keys = _.chain(items)
       .flatten()
       .uniq()
       .sort()
-      .transform( (obj, a) => {
-        obj[a] = "";
-      }, {} )
+      .transform((obj, a) => {
+        obj[a] = '';
+      }, {})
       .value();
 
-    console.log("We have "+ _.keys(keys).length + " i18n keys.");
+  console.log(`We have ${_.keys(keys).length} i18n keys.`);
 
-    _.each(i18nConfig.locales, lang => {
-        updateKeysToFile( keys, lang )
-    });
+  _.each(i18nConfig.locales, lang => {
+    updateKeysToFile(keys, lang);
+  });
 });
